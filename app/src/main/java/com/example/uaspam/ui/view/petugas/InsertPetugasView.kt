689a41ca9b373bfee2c7efeadae8f1b420aa2138
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-24 22:11:46 +0700

    Membuat fungsi EntryPtgScreen mengelola tampilan InsertPetugas

--- app/src/main/java/com/example/uaspam/ui/view/petugas/InsertPetugasView.kt
+++ app/src/main/java/com/example/uaspam/ui/view/petugas/InsertPetugasView.kt
@@ -17,24 +17,89 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.uaspam.data.Jabatan
+import com.example.uaspam.ui.customwidget.CostumeTopAppBar
 import com.example.uaspam.ui.customwidget.DynamicSelectedTextField
+import com.example.uaspam.ui.navigation.DestinasiInsertHewan
+import com.example.uaspam.ui.viewmodel.PenyediaViewModel
 import com.example.uaspam.ui.viewmodel.petugas.FormErrorStateptg
 import com.example.uaspam.ui.viewmodel.petugas.InsertPUiEvent
 import com.example.uaspam.ui.viewmodel.petugas.InsertPUiState
+import com.example.uaspam.ui.viewmodel.petugas.InsertPetugasViewModel
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryPtgScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertPetugasViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val uiState = viewModel.uiState
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    LaunchedEffect(uiState.snackBarMessage) {
+        uiState.snackBarMessage?.let { message ->
+            coroutineScope.launch {
+                snackbarHostState.showSnackbar(message)
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiInsertHewan.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+        EntryBody(
+            additionalText = "Harap Isi Semua Data !",
+            insertPUiState = viewModel.uiState,
+            onPetugasValueChange = viewModel::InsertPtgState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertPtg()
+                    navigateBack()
+                }
+            },
+            onCancelClick = {
+                navigateBack()
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxWidth()
+        )
+    }
+}
 
 @Composable
 fun EntryBody(
