db3436d2ae2b4075c12591de63c8b2f2d4366bf4
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-24 14:16:12 +0700

    Membuat fungsi DetaiHewanContent mengisi nilai DetailRow

--- app/src/main/java/com/example/uaspam/ui/view/hewan/DetailHewanView.kt
+++ app/src/main/java/com/example/uaspam/ui/view/hewan/DetailHewanView.kt
@@ -1,14 +1,34 @@
 package com.example.uaspam.ui.view.hewan
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.uaspam.model.Hewan
+
+@Composable
+fun DetailHewanContent(
+    modifier: Modifier = Modifier,
+    hewan: Hewan
+) {
+    Column (
+        modifier = modifier.fillMaxWidth().padding(20.dp),
+        horizontalAlignment = Alignment.Start
+    ) {
+        DetailRow(judul = "ID Hewan", value = hewan.Id_hewan)
+        DetailRow(judul = "Nama Hewan", value = hewan.Nama_hewan)
+        DetailRow(judul = "Tipe Pakan", value = hewan.Tipe_pakan)
+        DetailRow(judul = "Populasi", value = hewan.populasi.toString())
+        DetailRow(judul = "Zona Wilayah", value = hewan.Zona_wilayah)
+    }
+}
 
 
 @Composable
