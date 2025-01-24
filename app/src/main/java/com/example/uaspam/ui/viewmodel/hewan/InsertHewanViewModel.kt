a657ca5cde3ce3bf1bf077569b71af2c277e9a23
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-24 10:05:24 +0700

    Menambahkan FormErrorStatehwn yang memvalidasi input

--- app/src/main/java/com/example/uaspam/ui/viewmodel/hewan/InsertHewanViewModel.kt
+++ app/src/main/java/com/example/uaspam/ui/viewmodel/hewan/InsertHewanViewModel.kt
@@ -2,6 +2,18 @@ package com.example.uaspam.ui.viewmodel.hewan
 
 
 
+data class FormErrorStatehwn(
+    val Id_hewan: String? = null,
+    val Nama_hewan: String? = null,
+    val Tipe_pakan: String? = null,
+    val populasi: String? = null,
+    val Zona_wilayah: String? = null,
+) {
+    fun isValid(): Boolean {
+        return Id_hewan == null && Nama_hewan == null && Tipe_pakan == null &&
+                populasi  == null && Zona_wilayah == null
+    }
+}
 
 data class InsertUiEvent(
     val Id_hewan: String = "",
