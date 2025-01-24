fade15cffafee8f32d7adff8d923a8711aa054ae
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-24 09:59:18 +0700

    Membuat InsertUiEvent dengan nilainya kosong

---
+++ app/src/main/java/com/example/uaspam/ui/viewmodel/hewan/InsertHewanViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.uaspam.ui.viewmodel.hewan
+
+
+
+
+data class InsertUiEvent(
+    val Id_hewan: String = "",
+    val Nama_hewan: String = "",
+    val Tipe_pakan: String = "",
+    val populasi: Int = 0,
+    val Zona_wilayah: String = "",
+)
