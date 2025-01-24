950768c3491832d44b248a77ab4a8bf0c404f0db
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-24 16:28:18 +0700

    Menambahkan Destinasi untuk rute Petugas

--- app/src/main/java/com/example/uaspam/ui/navigation/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/uaspam/ui/navigation/DestinasiNavigasi.kt
@@ -33,3 +33,27 @@ object DestinasiUpdateHewan : DestinasiNavigasi {
     const val IDH = "Id_hewan"
     val routesWithArg = "$route/{$IDH}"
 }
+
+object DestinasiHomePetugas : DestinasiNavigasi{
+    override val route = "homePtg"
+    override val titleRes = "Home Petugas"
+}
+
+object DestinasiInsertPetugas : DestinasiNavigasi {
+    override val route = "insertPtg"
+    override val titleRes = "Entry Petugas"
+}
+
+object DestinasiDetailPetugas : DestinasiNavigasi {
+    override val route: String = "detailPtg"
+    override val titleRes: String = "Detail Petugas"
+    const val IDP = "Id_petugas"
+    val routesWithArg = "$route/{$IDP}"
+}
+
+object DestinasiUpdatePetugas : DestinasiNavigasi {
+    override val route: String = "updatePtg"
+    override val titleRes: String = "Update Petugas"
+    const val IDP = "Id_petugas"
+    val routesWithArg = "$route/{$IDP}"
+}
