b535aefd2278fa7425c259ab2d336f795a445f99
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-24 09:36:22 +0700

    Menambahkan class yang menghubungkan HewanRepository dengan service

--- app/src/main/java/com/example/uaspam/repository/HewanRepository.kt
+++ app/src/main/java/com/example/uaspam/repository/HewanRepository.kt
@@ -3,6 +3,8 @@ package com.example.uaspam.repository
 import com.example.uaspam.model.Hewan
 import com.example.uaspam.model.HewanDetailResponse
 import com.example.uaspam.model.HewanResponse
+import com.example.uaspam.service.HewanService
+import java.io.IOException
 
 interface HewanRepository {
     suspend fun getHewan(): HewanResponse
@@ -11,3 +13,38 @@ interface HewanRepository {
     suspend fun deleteHewan(Id_hewan: String)
     suspend fun getHewanbyId(Id_hewan: String): HewanDetailResponse
 }
+
+class NetworkHewanRepository(
+    private val hewanApiService: HewanService
+) : HewanRepository {
+    override suspend fun insertHewan(hewan: Hewan) {
+        hewanApiService.insertHewan(hewan)
+    }
+
+    override suspend fun updateHewan(Id_hewan: String, hewan: Hewan) {
+        hewanApiService.updateHewan(Id_hewan, hewan)
+    }
+
+    override suspend fun deleteHewan(Id_hewan: String) {
+        try{
+            val response = hewanApiService.deleteHewan(Id_hewan)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete hewan. HTTP Status code: " +
+                        "${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        }catch (e:Exception){
+            throw e
+        }
+    }
+
+    override suspend fun getHewan(): HewanResponse{
+        return hewanApiService.getHewan()
+    }
+    override suspend fun getHewanbyId(Id_hewan: String): HewanDetailResponse{
+        return hewanApiService.getHewanbyId(Id_hewan)
+    }
+}
+
