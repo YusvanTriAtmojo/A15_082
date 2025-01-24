ba842742db79cdb4ab38527a983de5dec8866638
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-24 16:11:10 +0700

    Menambahkan class yang mengintegrasikan PetugasRepository dengan PetugasService

--- app/src/main/java/com/example/uaspam/repository/PetugasRepository.kt
+++ app/src/main/java/com/example/uaspam/repository/PetugasRepository.kt
@@ -3,6 +3,8 @@ package com.example.uaspam.repository
 import com.example.uaspam.model.Petugas
 import com.example.uaspam.model.PetugasDetailResponse
 import com.example.uaspam.model.PetugasResponse
+import com.example.uaspam.service.PetugasService
+import java.io.IOException
 
 
 interface PetugasRepository {
@@ -11,4 +13,38 @@ interface PetugasRepository {
     suspend fun updatePetugas(Id_petugas: String, petugas: Petugas)
     suspend fun deletePetugas(Id_petugas: String)
     suspend fun getPetugasbyId(Id_petugas: String): PetugasDetailResponse
+}
+
+class NetworkPetugasRepository(
+    private val petugasApiService: PetugasService
+) : PetugasRepository {
+    override suspend fun insertPetugas(petugas: Petugas) {
+        petugasApiService.insertPetugas(petugas)
+    }
+
+    override suspend fun updatePetugas(Id_petugas: String, petugas: Petugas) {
+        petugasApiService.updatePetugas(Id_petugas, petugas)
+    }
+
+    override suspend fun deletePetugas(Id_petugas: String) {
+        try{
+            val response = petugasApiService.deletePetugas(Id_petugas)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete petugas. HTTP Status code: " +
+                        "${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        }catch (e:Exception){
+            throw e
+        }
+    }
+
+    override suspend fun getPetugas(): PetugasResponse {
+        return petugasApiService.getPetugas()
+    }
+    override suspend fun getPetugasbyId(Id_petugas: String): PetugasDetailResponse {
+        return petugasApiService.getPetugasbyId(Id_petugas)
+    }
 }
