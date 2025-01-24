cba6e06356af0abbe987742954e3ca0a18d2020e
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-24 20:31:59 +0700

    Menambahkan initializer home, insert, detail, update petugas viewmodel

--- app/src/main/java/com/example/uaspam/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/uaspam/ui/viewmodel/PenyediaViewModel.kt
@@ -10,6 +10,10 @@ import com.example.uaspam.ui.viewmodel.hewan.DetailHewanViewModel
 import com.example.uaspam.ui.viewmodel.hewan.HomeHewanViewModel
 import com.example.uaspam.ui.viewmodel.hewan.InsertHewanViewModel
 import com.example.uaspam.ui.viewmodel.hewan.UpdateHewanViewModel
+import com.example.uaspam.ui.viewmodel.petugas.DetailPetugasViewModel
+import com.example.uaspam.ui.viewmodel.petugas.HomePetugasViewModel
+import com.example.uaspam.ui.viewmodel.petugas.InsertPetugasViewModel
+import com.example.uaspam.ui.viewmodel.petugas.UpdatePetugasViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
@@ -32,6 +36,28 @@ object PenyediaViewModel {
         initializer {
             UpdateHewanViewModel(
                 aplikasiHwn().container.hewanRepository,
+                createSavedStateHandle(),
+            )
+        }
+        initializer {
+            HomePetugasViewModel(
+                aplikasiHwn().container.petugasRepository
+            )
+        }
+        initializer {
+            InsertPetugasViewModel(
+                aplikasiHwn().container.petugasRepository
+            )
+        }
+        initializer {
+            DetailPetugasViewModel(
+                createSavedStateHandle(),
+                aplikasiHwn().container.petugasRepository
+            )
+        }
+        initializer {
+            UpdatePetugasViewModel(
+                aplikasiHwn().container.petugasRepository,
                 createSavedStateHandle(),
             )
         }
