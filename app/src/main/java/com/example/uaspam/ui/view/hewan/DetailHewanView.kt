01bf263a48fdfad43c02079d03152a21cbf4f830
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-24 14:53:35 +0700

    fungsi DetailHewanScreen yang mengelola View DetailHewan

--- app/src/main/java/com/example/uaspam/ui/view/hewan/DetailHewanView.kt
+++ app/src/main/java/com/example/uaspam/ui/view/hewan/DetailHewanView.kt
@@ -1,5 +1,7 @@
 package com.example.uaspam.ui.view.hewan
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -7,11 +9,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -22,12 +31,71 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.uaspam.R
 import com.example.uaspam.model.Hewan
 import com.example.uaspam.model.HewanDetailResponse
+import com.example.uaspam.ui.customwidget.CostumeTopAppBar
+import com.example.uaspam.ui.viewmodel.PenyediaViewModel
 import com.example.uaspam.ui.viewmodel.hewan.DetailHUiState
+import com.example.uaspam.ui.viewmodel.hewan.DetailHewanViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailHewanScreen(
+    navigateBack: () -> Unit,
+    onDeleteClick: (String) -> Unit,
+    onEditClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailHewanViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    Scaffold(
+        topBar = {
+            CostumeTopAppBar(
+                title = "Detail Hewan",
+                canNavigateBack = true,
+                onRefresh = {
+                    viewModel.getHewanById()
+                },
+                navigateUp = navigateBack,
+                modifier = Modifier
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onEditClick,
+                shape = MaterialTheme.shapes.medium,
+                containerColor = Color(0xFF018786),
+                contentColor = Color.White,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(imageVector = Icons.Default.Edit, contentDescription = "Edit Hewan")
+            }
+        },
+    ) { innerPadding ->
+        Box (modifier = Modifier.fillMaxSize()) {
+            Image(
+                painter = painterResource(id = R.drawable.jungle),
+                contentDescription = "Background Image",
+                modifier = Modifier
+                    .fillMaxSize()
+            )
+            DetailHewanStatus(
+                modifier = Modifier.padding(innerPadding),
+                detailHUiState = viewModel.detailHUiState,
+                retryAction = { viewModel.getHewanById() },
+                onDeleteClick = { id ->
+                    viewModel.deleteHwn(id)
+                    onDeleteClick(id)
+                }
+            )
+        }
+    }
+}
 
 @Composable
 fun DetailHewanStatus(
