144214e7c1ce2bac4878ebdba4e9857bd3e619fa
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-24 19:23:17 +0700

    Membuat data class FormErrorStateptg validasi

--- app/src/main/java/com/example/uaspam/ui/viewmodel/petugas/InsertPetugasViewModel.kt
+++ app/src/main/java/com/example/uaspam/ui/viewmodel/petugas/InsertPetugasViewModel.kt
@@ -1,5 +1,14 @@
 package com.example.uaspam.ui.viewmodel.petugas
 
+data class FormErrorStateptg(
+    val Id_petugas: String? = null,
+    val Nama_petugas: String? = null,
+    val jabatan: String? = null,
+) {
+    fun isValid(): Boolean {
+        return Id_petugas == null && Nama_petugas == null && jabatan == null
+    }
+}
 
 data class InsertPUiEvent(
     val Id_petugas: String = "",
