9450ce7668547ddfa1af867215aadf2305019a91
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-24 14:25:19 +0700

    fungsi ItemDetailHwn yang membungkus DetailHewanContent dalam Card

--- app/src/main/java/com/example/uaspam/ui/view/hewan/DetailHewanView.kt
+++ app/src/main/java/com/example/uaspam/ui/view/hewan/DetailHewanView.kt
@@ -2,16 +2,73 @@ package com.example.uaspam.ui.view.hewan
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.uaspam.model.Hewan
+import com.example.uaspam.model.HewanDetailResponse
+
+@Composable
+fun ItemDetailHwn(
+    modifier: Modifier = Modifier,
+    onDeleteClick: () -> Unit,
+    hewan: HewanDetailResponse
+) {
+    Card (
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color(0xC0016D47),
+            contentColor = Color.White,
+        )
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = "Detail Hewan",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Card (
+                colors = CardDefaults.cardColors(
+                    containerColor = Color(0xFF018786),
+                    contentColor = Color.White
+                ),
+            ){
+                DetailHewanContent(hewan = hewan.data1)
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Button (
+                onClick = onDeleteClick,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFFD20A4E),
+                    contentColor = Color.White
+                ),
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "Delete")
+            }
+        }
+    }
+}
+
 
 @Composable
 fun DetailHewanContent(
