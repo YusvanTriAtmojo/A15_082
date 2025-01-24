695957e1b471ff93fb96e2bf25abf59719de32e2
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-24 17:35:10 +0700

    Membuat class HomePetugasViewModel yang mengambil list petugas

--- app/src/main/java/com/example/uaspam/ui/viewmodel/petugas/HomePetugasViewModel.kt
+++ app/src/main/java/com/example/uaspam/ui/viewmodel/petugas/HomePetugasViewModel.kt
@@ -1,10 +1,42 @@
 package com.example.uaspam.ui.viewmodel.petugas
 
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.uaspam.model.Petugas
+import com.example.uaspam.repository.PetugasRepository
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 sealed class HomePUiState {
     data class Success(val petugas: List<Petugas>): HomePUiState()
     object Error : HomePUiState()
     object Loading : HomePUiState()
 }
+
+class HomePetugasViewModel(private val ptg: PetugasRepository) : ViewModel() {
+    var ptgUiState: HomePUiState by mutableStateOf(HomePUiState.Loading)
+        private set
+
+    init {
+        getPtg()
+    }
+
+    fun getPtg(){
+        viewModelScope.launch {
+            ptgUiState = HomePUiState.Loading
+            ptgUiState = try {
+                HomePUiState.Success(ptg.getPetugas().data3)
+            } catch (e: IOException){
+                HomePUiState.Error
+            } catch (e: HttpException){
+                HomePUiState.Error
+            }
+        }
+    }
+}
+
