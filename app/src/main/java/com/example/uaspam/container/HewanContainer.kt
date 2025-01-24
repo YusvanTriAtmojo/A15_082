2375eb10a99db4483769408b3d2547339d7a71e3
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-24 16:34:52 +0700

    Menambahkan PetugasRepository ke dalam interface AppContainer

--- app/src/main/java/com/example/uaspam/container/HewanContainer.kt
+++ app/src/main/java/com/example/uaspam/container/HewanContainer.kt
@@ -1,8 +1,17 @@
 package com.example.uaspam.container
 
 import com.example.uaspam.repository.HewanRepository
+import com.example.uaspam.repository.KandangRepository
+import com.example.uaspam.repository.MonitoringRepository
 import com.example.uaspam.repository.NetworkHewanRepository
+import com.example.uaspam.repository.NetworkKandangRepository
+import com.example.uaspam.repository.NetworkMonitoringRepository
+import com.example.uaspam.repository.NetworkPetugasRepository
+import com.example.uaspam.repository.PetugasRepository
 import com.example.uaspam.service.HewanService
+import com.example.uaspam.service.KandangService
+import com.example.uaspam.service.MonitoringService
+import com.example.uaspam.service.PetugasService
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
@@ -10,6 +19,7 @@ import retrofit2.Retrofit
 
 interface AppContainer{
     val hewanRepository: HewanRepository
+    val petugasRepository: PetugasRepository
 }
 
 class HewanContainer: AppContainer {
