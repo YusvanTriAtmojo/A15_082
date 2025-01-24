a9ab6bf796bfa7b08b3ba87d5b0acb4e58947da6
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-24 10:35:01 +0700

    Menambahkan class InsertHewanViewModel yang menginsert data hewan

--- app/src/main/java/com/example/uaspam/ui/viewmodel/hewan/InsertHewanViewModel.kt
+++ app/src/main/java/com/example/uaspam/ui/viewmodel/hewan/InsertHewanViewModel.kt
@@ -1,6 +1,59 @@
 package com.example.uaspam.ui.viewmodel.hewan
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.uaspam.model.Hewan
+import com.example.uaspam.repository.HewanRepository
+import kotlinx.coroutines.launch
+
+class InsertHewanViewModel(
+    private val hwn: HewanRepository
+): ViewModel()
+{
+    var uiState by mutableStateOf(InsertUiState())
+        private set
+
+    fun InsertHwnState(insertUiEvent: InsertUiEvent){
+        uiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+    private fun validateFields(): Boolean {
+        val event = uiState.insertUiEvent
+        val errorState = FormErrorStatehwn(
+            Nama_hewan = if (event.Nama_hewan.isNotEmpty()) null else "Nama tidak boleh kosong",
+            Tipe_pakan = if (event.Tipe_pakan.isNotEmpty()) null else "Tipe pakan tidak boleh kosong",
+            populasi = if (event.populasi >= 0) null else "Populasi tidak boleh kosong",
+            Zona_wilayah = if (event.Zona_wilayah.isNotEmpty()) null else "Zona wilayah Suplier tidak boleh kosong"
+        )
+
+        uiState = uiState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
+    suspend fun insertHwn(){
+        if (validateFields()) {
+            viewModelScope.launch {
+                try {
+                    hwn.insertHewan(uiState.insertUiEvent.toHwn())
+                    uiState = uiState.copy(
+                        snackBarMessage = "Data berhasil disimpan")
+                } catch (e:Exception){
+                    e.printStackTrace()
+                }
+            }
+        } else {
+            uiState = uiState.copy(
+                snackBarMessage = "Input tidak valid. Periksa kembali data Anda."
+            )
+        }
+    }
+    fun resetSnackBarMessage(){
+        uiState = uiState.copy(snackBarMessage = null)
+    }
+}
+
 
 data class InsertUiState(
     val insertUiEvent: InsertUiEvent = InsertUiEvent(),
