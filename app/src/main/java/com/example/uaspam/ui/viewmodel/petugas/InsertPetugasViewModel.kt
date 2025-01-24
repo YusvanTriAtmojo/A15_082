7b76ab554a3c4373d382c5336f2748c285d21143
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-24 19:16:36 +0700

    Membuat data class InsertPuiEvent dengan variabel yang nilainya kosong

---
+++ app/src/main/java/com/example/uaspam/ui/viewmodel/petugas/InsertPetugasViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.uaspam.ui.viewmodel.petugas
+
+
+data class InsertPUiEvent(
+    val Id_petugas: String = "",
+    val Nama_petugas: String = "",
+    val jabatan: String = "",
+)
