779a89e91ea557c01c7813c9b0be6660ed899d16
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-24 10:48:03 +0700

    Membuat class yang mengelola mengambil data detail hewan

--- app/src/main/java/com/example/uaspam/ui/viewmodel/hewan/DetailHewanViewModel.kt
+++ app/src/main/java/com/example/uaspam/ui/viewmodel/hewan/DetailHewanViewModel.kt
@@ -1,10 +1,48 @@
 package com.example.uaspam.ui.viewmodel.hewan
 
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.uaspam.model.HewanDetailResponse
+import com.example.uaspam.repository.HewanRepository
+import com.example.uaspam.ui.navigation.DestinasiDetailHewan
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 sealed class DetailHUiState {
     data class Success(val hewan: HewanDetailResponse): DetailHUiState()
     object Error : DetailHUiState()
     object Loading : DetailHUiState()
+}
+
+class DetailHewanViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val hwn: HewanRepository
+) : ViewModel() {
+    var detailHUiState: DetailHUiState by mutableStateOf(DetailHUiState.Loading)
+        private set
+
+    private val id: String = checkNotNull(savedStateHandle[DestinasiDetailHewan.IDH])
+
+    init {
+        getHewanById()
+    }
+    fun getHewanById(){
+        viewModelScope.launch {
+            detailHUiState = DetailHUiState.Loading
+            detailHUiState = try {
+                val hewan = hwn.getHewanbyId(id)
+                DetailHUiState.Success(hewan)
+            } catch (e: IOException){
+                DetailHUiState.Error
+            } catch (e: HttpException) {
+                DetailHUiState.Error
+            }
+        }
+    }
 }
