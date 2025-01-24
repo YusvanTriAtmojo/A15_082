07ff79f7b8a943031aa2dacd287a222dc06745ac
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-24 22:25:22 +0700

    Menambahkan fungsi DetailPetugasContent mengisi nilai DetailRow

--- app/src/main/java/com/example/uaspam/ui/view/petugas/DetailPetugasView.kt
+++ app/src/main/java/com/example/uaspam/ui/view/petugas/DetailPetugasView.kt
@@ -1,15 +1,32 @@
 package com.example.uaspam.ui.view.petugas
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.uaspam.model.Petugas
 
+@Composable
+fun DetailPetugasContent(
+    modifier: Modifier = Modifier,
+    petugas: Petugas
+) {
+    Column(
+        modifier = modifier.fillMaxWidth().padding(20.dp),
+        horizontalAlignment = Alignment.Start
+    ) {
+        DetailRow(judul = "ID Petugas", value = petugas.Id_petugas)
+        DetailRow(judul = "Nama", value = petugas.Nama_petugas)
+        DetailRow(judul = "Jabatan", value = petugas.jabatan)
+    }
+}
 
 @Composable
 fun DetailRow(
