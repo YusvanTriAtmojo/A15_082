2b77388d2d62f698fc7ce6d5b455596cf35ebe0a
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-24 19:46:10 +0700

    Membuat class InsertPetugasViewModel yang menginsert data petugas

--- app/src/main/java/com/example/uaspam/ui/viewmodel/petugas/InsertPetugasViewModel.kt
+++ app/src/main/java/com/example/uaspam/ui/viewmodel/petugas/InsertPetugasViewModel.kt
@@ -1,7 +1,52 @@
 package com.example.uaspam.ui.viewmodel.petugas
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.uaspam.model.Petugas
+import com.example.uaspam.repository.PetugasRepository
+import kotlinx.coroutines.launch
 
+class InsertPetugasViewModel(private val ptg: PetugasRepository): ViewModel() {
+    var uiState by mutableStateOf(InsertPUiState())
+        private set
+
+    fun InsertPtgState(insertPUiEvent: InsertPUiEvent){
+        uiState = InsertPUiState(insertPUiEvent = insertPUiEvent)
+    }
+    private fun validateFields(): Boolean {
+        val event = uiState.insertPUiEvent
+        val errorState = FormErrorStateptg(
+            Id_petugas = if (event.Id_petugas.isNotEmpty()) null else "ID tidak boleh kosong",
+            Nama_petugas = if (event.Nama_petugas.isNotEmpty()) null else "Nama tidak boleh kosong",
+            jabatan = if (event.jabatan.isNotEmpty()) null else "Jabatan tidak boleh kosong",
+        )
+
+        uiState = uiState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
+    suspend fun insertPtg(){
+        if (validateFields()) {
+            viewModelScope.launch {
+                try {
+                    ptg.insertPetugas(uiState.insertPUiEvent.toPtg())
+                } catch (e:Exception){
+                    e.printStackTrace()
+                }
+            }
+        } else {
+            uiState = uiState.copy(
+                snackBarMessage = "Input tidak valid. Periksa kembali data Anda."
+            )
+        }
+    }
+    fun resetSnackBarMessage(){
+        uiState = uiState.copy(snackBarMessage = null)
+    }
+}
 
 data class InsertPUiState(
     val insertPUiEvent: InsertPUiEvent = InsertPUiEvent(),
