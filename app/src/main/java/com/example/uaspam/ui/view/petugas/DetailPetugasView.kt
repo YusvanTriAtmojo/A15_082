9aae70d805169017da89d23e674d2ff74ff37477
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-24 23:10:53 +0700

    Membuat fungsi DetailPetugasScreen mengelola view DetailPetugas dan perbaikan di OnError

--- app/src/main/java/com/example/uaspam/ui/view/petugas/DetailPetugasView.kt
+++ app/src/main/java/com/example/uaspam/ui/view/petugas/DetailPetugasView.kt
@@ -1,6 +1,8 @@
 package com.example.uaspam.ui.view.petugas
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -16,8 +18,10 @@ import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -28,12 +32,61 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.uaspam.R
 import com.example.uaspam.model.Petugas
 import com.example.uaspam.model.PetugasDetailResponse
+import com.example.uaspam.ui.customwidget.CostumeTopAppBar
+import com.example.uaspam.ui.viewmodel.PenyediaViewModel
 import com.example.uaspam.ui.viewmodel.petugas.DetailPUiState
+import com.example.uaspam.ui.viewmodel.petugas.DetailPetugasViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailPetugasScreen(
+    navigateBack: () -> Unit,
+    onDeleteClick: (String) -> Unit,
+    onEditClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailPetugasViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    Scaffold(
+        topBar = {
+            CostumeTopAppBar(
+                title = "Detail Petugas",
+                canNavigateBack = true,
+                onRefresh = {
+                    viewModel.getPetugasById()
+                },
+                navigateUp = navigateBack,
+                modifier = Modifier
+            )
+        },
+    ) { innerPadding ->
+        Box(modifier = Modifier.fillMaxSize()) {
+            Image(
+                painter = painterResource(id = R.drawable.keeper1),
+                contentDescription = "Background Image",
+                modifier = Modifier
+                    .fillMaxSize(),
+            )
+            DetailPetugasStatus(
+                modifier = Modifier.padding(innerPadding),
+                detailPUiState = viewModel.detailPUiState,
+                retryAction = { viewModel.getPetugasById() },
+                onEditClick = onEditClick,
+                onDeleteClick = { id ->
+                    viewModel.deletePtg(id)
+                    onDeleteClick(id)
+                }
+            )
+        }
+    }
+}
 
 @Composable
 fun DetailPetugasStatus(
@@ -59,7 +112,7 @@ fun DetailPetugasStatus(
                 modifier = modifier.fillMaxWidth()
             )
         }
-        is DetailPUiState.Error -> com.example.uaspam.ui.view.hewan.OnError(
+        is DetailPUiState.Error -> OnError(
             retryAction,
             modifier = modifier.fillMaxSize()
         )
