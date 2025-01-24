d5906e87e83ccaadbb00286477957f0b39c7ab52
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-24 09:53:21 +0700

    Membuat class getHwn yang akan mengambil list hewan

--- app/src/main/java/com/example/uaspam/ui/viewmodel/hewan/HomeHewanViewModel.kt
+++ app/src/main/java/com/example/uaspam/ui/viewmodel/hewan/HomeHewanViewModel.kt
@@ -1,11 +1,43 @@
 package com.example.uaspam.ui.viewmodel.hewan
 
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.uaspam.model.Hewan
+import com.example.uaspam.repository.HewanRepository
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 sealed class HomeHUiState {
     data class Success(val hewan: List<Hewan>): HomeHUiState()
     object Error : HomeHUiState()
     object Loading : HomeHUiState()
 }
+
+class HomeHewanViewModel(private val hwn: HewanRepository) : ViewModel() {
+    var hwnUiState: HomeHUiState by mutableStateOf(HomeHUiState.Loading)
+        private set
+
+    init {
+        getHwn()
+    }
+
+    fun getHwn(){
+        viewModelScope.launch {
+            hwnUiState = HomeHUiState.Loading
+            hwnUiState = try {
+                HomeHUiState.Success(hwn.getHewan().data1)
+            } catch (e: IOException){
+                HomeHUiState.Error
+            } catch (e: HttpException){
+                HomeHUiState.Error
+            }
+        }
+    }
+}
+
 
