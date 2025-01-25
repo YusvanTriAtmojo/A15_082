d33d4edeb76defce1def3b3bef406b60ab2567e2
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-25 21:58:08 +0700

    Membuat class InsertKandangViewModel yang mengelola insert data Kandang

--- app/src/main/java/com/example/uaspam/ui/viewmodel/kandang/InsertKandangViewModel.kt
+++ app/src/main/java/com/example/uaspam/ui/viewmodel/kandang/InsertKandangViewModel.kt
@@ -1,6 +1,55 @@
 package com.example.uaspam.ui.viewmodel.kandang
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.uaspam.model.Kandang
+import com.example.uaspam.repository.KandangRepository
+import kotlinx.coroutines.launch
+
+class InsertKandangViewModel(private val kdg: KandangRepository): ViewModel() {
+    var uiState by mutableStateOf(InsertKUiState())
+        private set
+
+    fun InsertKdgState(insertUiEvent: InsertKUiEvent){
+        uiState = InsertKUiState(insertKUiEvent = insertUiEvent)
+    }
+    private fun validateFields(): Boolean {
+        val event = uiState.insertKUiEvent
+        val errorState = FormErrorStatekdg(
+            Id_hewan = if (event.Id_hewan.isNotEmpty()) null else "ID hewan tidak boleh kosong",
+            kapasitas = if (event.kapasitas >= 0) null else "Kapasitas tidak boleh kosong",
+            lokasi = if (event.lokasi.isNotEmpty()) null else "Lokasi tidak boleh kosong",
+        )
+
+        uiState = uiState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
+    suspend fun insertKdg(){
+        if (validateFields()) {
+            viewModelScope.launch {
+                try {
+                    kdg.insertKandang(uiState.insertKUiEvent.toKdg())
+                    uiState = uiState.copy(
+                        snackBarMessage = "Data berhasil disimpan")
+                } catch (e:Exception){
+                    e.printStackTrace()
+                }
+            }
+        } else {
+            uiState = uiState.copy(
+                snackBarMessage = "Input tidak valid. Periksa kembali data Anda."
+            )
+        }
+    }
+    fun resetSnackBarMessage(){
+        uiState = uiState.copy(snackBarMessage = null)
+    }
+}
+
 
 data class InsertKUiState(
     val insertKUiEvent: InsertKUiEvent = InsertKUiEvent(),
