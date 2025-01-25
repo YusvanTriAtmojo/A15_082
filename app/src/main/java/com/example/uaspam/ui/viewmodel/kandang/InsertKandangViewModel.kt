704a0bb6c621b03fdacf189804d3044b3db1b307
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-25 21:28:19 +0700

    Membuat data class InsertKUiEvent dengan nilai val kosong

---
+++ app/src/main/java/com/example/uaspam/ui/viewmodel/kandang/InsertKandangViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.uaspam.ui.viewmodel.kandang
+
+
+data class InsertKUiEvent(
+    val Id_kandang: String = "",
+    val Id_hewan: String = "",
+    val kapasitas: Int = 0,
+    val lokasi: String = "",
+)
