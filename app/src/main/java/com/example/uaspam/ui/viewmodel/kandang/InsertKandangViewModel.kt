f23d4417b4872e2ef8f82e7da5e1e24cbf860341
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-25 21:33:45 +0700

    Menambahkan data class FormErrorStatekdg validasi nilai

--- app/src/main/java/com/example/uaspam/ui/viewmodel/kandang/InsertKandangViewModel.kt
+++ app/src/main/java/com/example/uaspam/ui/viewmodel/kandang/InsertKandangViewModel.kt
@@ -1,5 +1,16 @@
 package com.example.uaspam.ui.viewmodel.kandang
 
+data class FormErrorStatekdg(
+    val Id_kandang: String? = null,
+    val Id_hewan: String? = null,
+    val kapasitas: String? = null,
+    val lokasi: String? = null,
+) {
+    fun isValid(): Boolean {
+        return Id_kandang == null && Id_hewan == null && kapasitas == null &&
+                lokasi == null
+    }
+}
 
 data class InsertKUiEvent(
     val Id_kandang: String = "",
