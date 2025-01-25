96829bb20ddd5286ce9b05b5b766bcbcdecf1d2e
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-25 21:18:54 +0700

    Membuat class HomeKandangViewModel yang mengambil list kandang

--- app/src/main/java/com/example/uaspam/ui/viewmodel/kandang/HomeKandangViewModel.kt
+++ app/src/main/java/com/example/uaspam/ui/viewmodel/kandang/HomeKandangViewModel.kt
@@ -1,10 +1,41 @@
 package com.example.uaspam.ui.viewmodel.kandang
 
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.uaspam.model.Kandang
+import com.example.uaspam.repository.KandangRepository
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 sealed class HomeKUiState {
     data class Success(val kandang: List<Kandang>): HomeKUiState()
     object Error : HomeKUiState()
     object Loading : HomeKUiState()
+}
+
+class HomeKandangViewModel(private val kdg: KandangRepository) : ViewModel() {
+    var kdgUiState: HomeKUiState by mutableStateOf(HomeKUiState.Loading)
+        private set
+
+    init {
+        getKdg()
+    }
+
+    fun getKdg(){
+        viewModelScope.launch {
+            kdgUiState = HomeKUiState.Loading
+            kdgUiState = try {
+                HomeKUiState.Success(kdg.getKandang().data2)
+            } catch (e: IOException){
+                HomeKUiState.Error
+            } catch (e: HttpException){
+                HomeKUiState.Error
+            }
+        }
+    }
 }
