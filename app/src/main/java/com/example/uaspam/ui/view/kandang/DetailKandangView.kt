156669558424590b4c53ff09a787501475403504
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-26 09:38:54 +0700

    Menambahkan fungsi DetailKandangContent yang mengisi nilai DetailRow

--- app/src/main/java/com/example/uaspam/ui/view/kandang/DetailKandangView.kt
+++ app/src/main/java/com/example/uaspam/ui/view/kandang/DetailKandangView.kt
@@ -1,14 +1,34 @@
 package com.example.uaspam.ui.view.kandang
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.uaspam.model.KandangHewan
+
+@Composable
+fun DetailKandangContent(
+    modifier: Modifier = Modifier,
+    kandang: KandangHewan,
+) {
+    Column(
+        modifier = modifier.fillMaxWidth().padding(20.dp),
+        horizontalAlignment = Alignment.Start
+    ) {
+        DetailRow(judul = "ID Kandang", value = kandang.Id_kandang)
+        DetailRow(judul = "ID Hewan", value = kandang.Id_hewan)
+        DetailRow(judul = "Nama Hewan", value = kandang.Nama_hewan)
+        DetailRow(judul = "Kapasitas", value = kandang.kapasitas.toString())
+        DetailRow(judul = "Lokasi", value = kandang.lokasi)
+    }
+}
 
 @Composable
 fun DetailRow(
