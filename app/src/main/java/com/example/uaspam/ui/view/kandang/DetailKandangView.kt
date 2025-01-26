e336f4b9a994eafaa5721e00c9f25c8b33ca0b28
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-26 09:45:57 +0700

    Menambahkan fungsi ItemDetailKdg memasukkan DetailKandangContent ke Card

--- app/src/main/java/com/example/uaspam/ui/view/kandang/DetailKandangView.kt
+++ app/src/main/java/com/example/uaspam/ui/view/kandang/DetailKandangView.kt
@@ -1,17 +1,99 @@
 package com.example.uaspam.ui.view.kandang
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.uaspam.model.KandangDetailResponse
 import com.example.uaspam.model.KandangHewan
+
+@Composable
+fun ItemDetailKdg(
+    modifier: Modifier = Modifier,
+    onEditClick: () -> Unit,
+    onDeleteClick: () -> Unit,
+    kandang: KandangDetailResponse,
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color(0xC0016D47),
+            contentColor = Color.White,
+        )
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = "Detail Kandang",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Card (
+                colors = CardDefaults.cardColors(
+                    containerColor = Color(0xFF018786),
+                    contentColor = Color.White
+                ),
+            ){
+                DetailKandangContent(kandang = kandang.data2)
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Row (modifier = Modifier.padding(start = 128.dp)) {
+                IconButton(
+                    onClick = onDeleteClick,
+                    modifier = Modifier
+                        .size(50.dp)
+                        .background(Color(0xFFD20A4E), shape = RoundedCornerShape(10.dp))
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = "Delete",
+                        tint = Color.White,
+                        modifier = Modifier.size(30.dp)
+                    )
+                }
+                Spacer(modifier = Modifier.padding(10.dp))
+                IconButton(
+                    onClick = onEditClick,
+                    modifier = Modifier
+                        .size(50.dp)
+                        .background(Color(0xFFFFB300), shape = RoundedCornerShape(10.dp))
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Edit,
+                        contentDescription = "Edit",
+                        tint = Color.White,
+                        modifier = Modifier.size(30.dp)
+                    )
+                }
+            }
+        }
+    }
+}
 
 @Composable
 fun DetailKandangContent(
