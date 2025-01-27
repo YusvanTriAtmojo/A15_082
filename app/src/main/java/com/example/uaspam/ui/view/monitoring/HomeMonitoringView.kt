1ab06e50d6d83166f78c41b7ad4b83442607b9d1
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-27 23:24:24 +0700

    menambahkan MtrLayout berupa items MtrCard yang dapat di scroll

--- app/src/main/java/com/example/uaspam/ui/view/monitoring/HomeMonitoringView.kt
+++ app/src/main/java/com/example/uaspam/ui/view/monitoring/HomeMonitoringView.kt
@@ -3,12 +3,15 @@ package com.example.uaspam.ui.view.monitoring
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.List
@@ -24,6 +27,28 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.uaspam.model.Monitoring
+
+@Composable
+fun MtrLayout(
+    monitoring: List<Monitoring>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Monitoring) -> Unit,
+){
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(15.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ){
+        items(monitoring) {mtr ->
+            MtrCard(
+                monitoring = mtr,
+                onDetailClick = { onDetailClick(mtr) },
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+        }
+    }
+}
 
 @Composable
 fun MtrCard(
