4662e689494a289fc229d261d8a6cd360b18dea6
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-27 21:25:43 +0700

    Membuat class DetailMonitoringViewModel yang mengambil satu data dan hapus

--- app/src/main/java/com/example/uaspam/ui/viewmodel/monitoring/DetailMonitoringViewModel.kt
+++ app/src/main/java/com/example/uaspam/ui/viewmodel/monitoring/DetailMonitoringViewModel.kt
@@ -1,9 +1,57 @@
 package com.example.uaspam.ui.viewmodel.monitoring
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.uaspam.model.MonitoringDetailResponse
+import com.example.uaspam.repository.MonitoringRepository
+import com.example.uaspam.ui.navigation.DestinasiDetailMonitoring
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 sealed class DetailMUiState {
     data class Success(val monitoring: MonitoringDetailResponse): DetailMUiState()
     object Error : DetailMUiState()
     object Loading : DetailMUiState()
+}
+
+class DetailMonitoringViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val mtr: MonitoringRepository
+) : ViewModel() {
+    var detailMUiState: DetailMUiState by mutableStateOf(DetailMUiState.Loading)
+        private set
+
+    private val id: String = checkNotNull(savedStateHandle[DestinasiDetailMonitoring.IDM])
+
+    init {
+        getMonitoringById()
+    }
+
+    fun getMonitoringById(){
+        viewModelScope.launch {
+            detailMUiState = DetailMUiState.Loading
+            detailMUiState = try {
+                val monitoring = mtr.getMonitoringbyId(id)
+                DetailMUiState.Success(monitoring)
+            } catch (e: IOException){
+                DetailMUiState.Error
+            }
+        }
+    }
+    fun deleteMtr(id: String){
+        viewModelScope.launch {
+            try {
+                mtr.deleteMonitoring(id)
+            } catch (e: IOException){
+                DetailMUiState.Error
+            } catch (e: HttpException) {
+                DetailMUiState.Error
+            }
+        }
+    }
 }
