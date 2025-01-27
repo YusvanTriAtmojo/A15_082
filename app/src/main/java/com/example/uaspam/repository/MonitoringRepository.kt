03502e10e6ae40b0cd72b15f495b2c7a5c08e9eb
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-27 19:39:13 +0700

    NetworkMonitoringRepository mengintegrasikan service dengan repo

--- app/src/main/java/com/example/uaspam/repository/MonitoringRepository.kt
+++ app/src/main/java/com/example/uaspam/repository/MonitoringRepository.kt
@@ -3,6 +3,8 @@ package com.example.uaspam.repository
 import com.example.uaspam.model.Monitoring
 import com.example.uaspam.model.MonitoringDetailResponse
 import com.example.uaspam.model.MonitoringResponse
+import com.example.uaspam.service.MonitoringService
+import java.io.IOException
 
 interface MonitoringRepository {
     suspend fun getMonitoring(): MonitoringResponse
@@ -11,4 +13,38 @@ interface MonitoringRepository {
     suspend fun deleteMonitoring(Id_monitoring: String)
     suspend fun getMonitoringbyId(Id_monitoring: String): MonitoringDetailResponse
 
+}
+
+class NetworkMonitoringRepository(
+    private val monitoringApiService: MonitoringService
+) : MonitoringRepository {
+    override suspend fun insertMonitoring(monitoring: Monitoring) {
+        monitoringApiService.insertMonitoring(monitoring)
+    }
+
+    override suspend fun updateMonitoring(Id_monitoring: String, monitoring: Monitoring) {
+        monitoringApiService.updateMonitoring(Id_monitoring, monitoring)
+    }
+
+    override suspend fun deleteMonitoring(Id_monitoring: String) {
+        try{
+            val response = monitoringApiService.deleteMonitoring(Id_monitoring)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete monitoring. HTTP Status code: " +
+                        "${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        }catch (e:Exception){
+            throw e
+        }
+    }
+
+    override suspend fun getMonitoring(): MonitoringResponse {
+        return monitoringApiService.getMonitoring()
+    }
+    override suspend fun getMonitoringbyId(Id_monitoring: String): MonitoringDetailResponse {
+        return monitoringApiService.getMonitoringbyId(Id_monitoring)
+    }
 }
