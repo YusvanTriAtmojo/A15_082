c6d1fcebea4f84f7d971aefe640d791c20031238
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-27 21:04:25 +0700

    Membuat class InsertMonitoringViewModel yang mengelola insert data monitoring

--- app/src/main/java/com/example/uaspam/ui/viewmodel/monitoring/InsertMonitoringViewModel.kt
+++ app/src/main/java/com/example/uaspam/ui/viewmodel/monitoring/InsertMonitoringViewModel.kt
@@ -1,6 +1,77 @@
 package com.example.uaspam.ui.viewmodel.monitoring
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.uaspam.model.Monitoring
+import com.example.uaspam.repository.MonitoringRepository
+import kotlinx.coroutines.launch
+
+class InsertMonitoringViewModel(private val mtr: MonitoringRepository) : ViewModel() {
+    var uiState by mutableStateOf(InsertMUiState())
+        private set
+
+    fun InsertMtrState(insertMUiEvent: InsertMUiEvent) {
+        updateStatus()
+
+        uiState = uiState.copy(
+            insertMUiEvent = insertMUiEvent.copy(Status = uiState.insertMUiEvent.Status)
+        )
+    }
+
+    private fun updateStatus() {
+        val event = uiState.insertMUiEvent
+        val totalPopulasi = event.Hewan_sakit + event.Hewan_sehat
+        val persentaseSakit = if (totalPopulasi > 0) (event.Hewan_sakit.toDouble() / totalPopulasi) * 100 else 0.0
+
+        val status = when {
+            persentaseSakit < 10 -> "Aman"
+            persentaseSakit in 11.0..50.0 -> "Waspada"
+            persentaseSakit > 51 -> "Kritis"
+            else -> "Aman"
+        }
+
+        uiState = uiState.copy(
+            insertMUiEvent = uiState.insertMUiEvent.copy(Status = status)
+        )
+    }
+
+    private fun validateFields(): Boolean {
+        val event = uiState.insertMUiEvent
+        val errorState = FormErrorStatemtr(
+            Id_monitoring = if (event.Id_monitoring.isNotEmpty()) null else "ID tidak boleh kosong",
+            Id_kandang = if (event.Id_kandang.isNotEmpty()) null else "ID kandang tidak boleh kosong",
+            Id_petugas = if (event.Id_petugas.isNotEmpty()) null else "ID petugas tidak boleh kosong",
+            Hewan_sakit = if (event.Hewan_sakit >= 0) null else "Hewan Sakit tidak boleh kosong",
+            Hewan_sehat = if (event.Hewan_sehat >= 0) null else "Hewan Sehat tidak boleh kosong",
+            Status = if (event.Status.isNotEmpty()) null else "Status tidak boleh kosong",
+        )
+        uiState = uiState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
+    suspend fun insertMtr() {
+        if (validateFields()) {
+            viewModelScope.launch {
+                try {
+                    mtr.insertMonitoring(uiState.insertMUiEvent.toMtr())
+                    uiState = uiState.copy(snackBarMessage = "Data berhasil disimpan")
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        } else {
+            uiState = uiState.copy(
+                snackBarMessage = "Input tidak valid. Periksa kembali data Anda."
+            )
+        }
+    }
+    fun resetSnackBarMessage() {
+        uiState = uiState.copy(snackBarMessage = null)
+    }
+}
 
 data class InsertMUiState(
     val insertMUiEvent: InsertMUiEvent = InsertMUiEvent(),
