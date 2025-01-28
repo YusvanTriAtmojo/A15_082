248442d95327266d736d77027767976c5006d067
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-28 18:33:20 +0700

    Memperbarui object DaftarHewan dengan filter hewan yang belum terpakai

--- app/src/main/java/com/example/uaspam/data/DaftarHewan.kt
+++ app/src/main/java/com/example/uaspam/data/DaftarHewan.kt
@@ -5,17 +5,32 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.uaspam.ui.viewmodel.PenyediaViewModel
 import com.example.uaspam.ui.viewmodel.hewan.HomeHUiState
 import com.example.uaspam.ui.viewmodel.hewan.HomeHewanViewModel
+import com.example.uaspam.ui.viewmodel.kandang.HomeKUiState
+import com.example.uaspam.ui.viewmodel.kandang.HomeKandangViewModel
 
 object DaftarHewan {
     @Composable
     fun ListHewan(
-        daftarHwn: HomeHewanViewModel = viewModel(factory = PenyediaViewModel.Factory)
+        daftarHwn: HomeHewanViewModel = viewModel(factory = PenyediaViewModel.Factory),
+        kandangVm: HomeKandangViewModel = viewModel(factory = PenyediaViewModel.Factory)
     ): List<Pair<String, String>> {
         val daftarhewan = daftarHwn.hwnUiState
+        val kandangState = kandangVm.kdgUiState
+
+        // mengambil id hewan yang sudah terpakai
+        val idHewanTerpakai = if (kandangState is HomeKUiState.Success) {
+            kandangState.kandang.map { it.Id_hewan }
+        } else {
+            emptyList()
+        }
         return if (daftarhewan is HomeHUiState.Success) {
-            daftarhewan.hewan.map { Pair(it.Id_hewan, it.Nama_hewan) }
+            // Filter berdasarkan yang belum terpakai
+            daftarhewan.hewan
+                .filter { it.Id_hewan !in idHewanTerpakai }
+                .map { Pair(it.Id_hewan, it.Nama_hewan) }
         } else {
             emptyList()
         }
     }
 }
+
