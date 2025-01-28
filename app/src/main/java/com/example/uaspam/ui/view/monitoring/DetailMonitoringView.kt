c5d347acad9dc80c1a22313c2882e921763bf433
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-28 11:43:33 +0700

    Mengisi nilai DetailRow melalui fungsi DetailMonitoringContent

--- app/src/main/java/com/example/uaspam/ui/view/monitoring/DetailMonitoringView.kt
+++ app/src/main/java/com/example/uaspam/ui/view/monitoring/DetailMonitoringView.kt
@@ -1,14 +1,38 @@
 package com.example.uaspam.ui.view.monitoring
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.uaspam.model.MonitoringFull
+
+@Composable
+fun DetailMonitoringContent(
+    modifier: Modifier = Modifier,
+    monitoring: MonitoringFull,
+) {
+    Column(
+        modifier = modifier.fillMaxWidth().padding(10.dp),
+        horizontalAlignment = Alignment.Start
+    ) {
+        DetailRow(judul = "ID Monitoring", value = monitoring.Id_monitoring)
+        DetailRow(judul = "ID Petugas", value = monitoring.Id_petugas)
+        DetailRow(judul = "Petugas", value = monitoring.Nama_petugas)
+        DetailRow(judul = "Nama Hewan", value = monitoring.Nama_hewan)
+        DetailRow(judul = "ID Kandang", value = monitoring.Id_kandang)
+        DetailRow(judul = "Tanggal", value = monitoring.Tanggal_monitoring)
+        DetailRow(judul = "Hewan Sakit", value = monitoring.Hewan_sakit.toString())
+        DetailRow(judul = "Hewan Sehat", value = monitoring.Hewan_sehat.toString())
+        DetailRow(judul = "Status", value = monitoring.Status)
+    }
+}
 
 @Composable
 fun DetailRow(
