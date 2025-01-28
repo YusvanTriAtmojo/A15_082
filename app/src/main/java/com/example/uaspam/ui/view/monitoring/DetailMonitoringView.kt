2978ce39a99a8b3df679f44e2a05aa9300b7d99d
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-01-28 11:25:50 +0700

    Membuat fungsi DetailRow dengan parameter judul, value

---
+++ app/src/main/java/com/example/uaspam/ui/view/monitoring/DetailMonitoringView.kt
@@ -0,0 +1,36 @@
+package com.example.uaspam.ui.view.monitoring
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun DetailRow(
+    modifier: Modifier = Modifier,
+    judul: String,
+    value: String
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp)
+    ) {
+        Text(
+            text = "$judul ",
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.weight(1f)
+        )
+        Text(
+            text = value,
+            fontSize = 16.sp,
+            modifier = Modifier.weight(2f)
+        )
+    }
+}
